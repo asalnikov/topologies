aca54fd38da6ebd33f1a150f9012b22cd5e7068d
parport0 <parport0@protonmail.com>
2020-04-20 16:09:57 +0300

    .

--- vis.py
+++ vis.py
@@ -1,45 +1,40 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
 import tempfile
-from graph_tool.all import *
+import argparse
+import graph_tool
+import graph_tool.draw
 from wrapper import Topologies
 
-if (len(sys.argv) != 2) and (len(sys.argv) != 3):
-    sys.exit("usage: vis.py <config.json> [ sfdp | fr | arf | plan | rand ]");
+parser = argparse.ArgumentParser()
+parser.add_argument('file', nargs='+', type=argparse.FileType('r'))
+parser.add_argument('--layout', nargs='?', const='sfdp', default='sfdp',
+    choices=["sfdp","fr","arf","rand"])
 
-try:
-    with open(sys.argv[1], 'r') as cfile:
-        content = cfile.read()
-except:
-    sys.exit("couldn't open file");
+args = parser.parse_args()
+content = ""
+for f in args.file:
+    content += f.read()
 
 l = Topologies(content)
 dot = l.network_parse(True, False)
 fp = tempfile.NamedTemporaryFile()
 fp.write(dot.encode())
 fp.seek(0)
-g = load_graph(fp.name, fmt='dot')
+g = graph_tool.load_graph(fp.name, fmt='dot')
 
-if len(sys.argv) == 3:
-    if sys.argv[2] == "sfdp":
-        pos = sfdp_layout(g)
-    elif sys.argv[2] == "fr":
-        pos = fruchterman_reingold_layout(g)
-    elif sys.argv[2] == "arf":
-        pos = arf_layout(g)
-    elif sys.argv[2] == "rad":
-        pos = radial_tree_layout(g)
-    elif sys.argv[2] == "rand":
-        pos = random_layout(g)
-    else:
-        sys.exit("no such layout algorithm");
+if args.layout == "fr":
+    pos = graph_tool.draw.fruchterman_reingold_layout(g)
+elif args.layout == "arf":
+    pos = graph_tool.draw.arf_layout(g)
+elif args.layout == "rand":
+    pos = graph_tool.draw.random_layout(g)
 else:
-    pos = sfdp_layout(g);
+    pos = graph_tool.draw.sfdp_layout(g)
 
 vprops = { 'text': g.vertex_properties["label"] }
 
-graph_draw(g, pos=pos, vprops=vprops, vertex_size=10, vertex_font_size=10,
-vertex_text_position=1, vertex_aspect=1, vertex_anchor=0, edge_pen_width=1,
-output_size=[4024,4024])
+graph_tool.draw.graph_draw(g, pos=pos, vprops=vprops, vertex_size=10,
+vertex_font_size=10, vertex_text_position=1, vertex_aspect=1, vertex_anchor=0,
+edge_pen_width=1, output_size=[4024,4024])
